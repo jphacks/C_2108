d758338fdef68e348d94213a529fb6b31f9e2c91
sakiho <honkon@fuji.waseda.jp>
2021-10-26 22:20:37 +0900

    firestore連携

--- reply-is-all-you-need/src/App.vue
+++ reply-is-all-you-need/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Add />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Add from './components/Add.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Add,
   },
 }
 </script>
--- reply-is-all-you-need/src/main.js
+++ reply-is-all-you-need/src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import firebase from 'firebase'
+
 Vue.config.productionTip = false
+const firebaseConfig = {
+  apiKey: 'AIzaSyDw8BWCt1m9zcjYc_sIvPngZeqFvnGiweg',
+  authDomain: 'reply-is-all-you-need.firebaseapp.com',
+  projectId: 'reply-is-all-you-need',
+  storageBucket: 'reply-is-all-you-need.appspot.com',
+  messagingSenderId: '844962270949',
+  appId: '1:844962270949:web:f2fa37d5dbe750ed674e0c',
+  measurementId: 'G-9SP9QYRXKC',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   render: h => h(App),
