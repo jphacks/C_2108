5763d0178f9c2946fce3371960c2e36779a9aa45
BuildTools <unconfigured@null.spigotmc.org>
2021-10-29 22:31:18 +0900

    画面右下のAdd buttonの追加

--- reply-is-all-you-need/src/components/Home.vue
+++ reply-is-all-you-need/src/components/Home.vue
@@ -1,23 +1,44 @@
 <template>
   <v-app id="app">
-    <Appbar />
+    <v-app-bar
+      color="#6A76AB"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      fade-img-on-scroll
+      app
+    >
+      <template #img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        />
+      </template>
+      <v-app-bar-title>Reply is All you need</v-app-bar-title>
+      <v-spacer />
+      <v-btn class="mt-3" text>
+        Add memo
+      </v-btn>
+    </v-app-bar>
     <v-main>
       <Add />
       <Timeline />
+      <v-btn class="mb-12" color="pink" fixed bottom dark absolute right fab>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import firebase from 'firebase/app'
-import Appbar from '@/components/Appbar.vue'
 import Add from '@/components/Add.vue'
 import Timeline from '@/components/Timeline.vue'
 
 export default {
   name: 'App',
   components: {
-    Appbar,
     Add,
     Timeline,
   },
