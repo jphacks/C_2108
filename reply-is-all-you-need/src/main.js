5b0e9d50a9377b96149f2dfadd87f8c784f02a97
BuildTools <unconfigured@null.spigotmc.org>
2021-10-29 21:30:03 +0900

    Update Timeline

--- reply-is-all-you-need/src/main.js
+++ reply-is-all-you-need/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import firebase from '@/plugins/firebase'
 import router from '@/router'
+import moment from 'moment'
 // UIコンポーネントの読み込み
 import vuetify from '@/plugins/vuetify'
 
 Vue.config.productionTip = false
+
+Vue.use(moment)
 
 new Vue({
   vuetify,
